27ff209ad17f54c78993de6e8660057956f69d58
Anonymous ethc4 <cs7778503@gmail.com>
2024-10-13 18:44:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fget',  # The name of your package on PyPI
+    name='fileget',  # The name of your package on PyPI
     version='0.1.0',  # Version of the package
     description='A powerful file downloader with FTP, HTTP/HTTPS support and advanced features like custom headers, SSL, and proxy.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/fget',  # Replace with your repo URL
+    author='Anonymousethc4',
+    author_email='cs7778503@gmail.com',
+    url='https://github.com/0625963141-cyber',  # Replace with your repo URL
     license='MIT',  # Choose your preferred license
     packages=find_packages(),  # Automatically finds your package
     install_requires=[
@@ -16,7 +16,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'fget=fget.fget:main',  # Exposes fget as a CLI command
+            'fileget=fileget.fileget:main',  # Exposes fget as a CLI command
         ],
     },
     classifiers=[
